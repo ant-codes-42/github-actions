660d9556e6951b7d183e2e5ab463d95861230906
ant-codes-42 <schwab.it@pm.me>
2025-03-05 18:59:08 -0600

    corrections for seed process

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,6 +1,5 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
-import cleanDB from "./cleanDb.js";
 
 import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
 
